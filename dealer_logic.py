b74626f5bf068c6d4f5d7a792dd7f174d7cf1545
sp-merrow <87612795+sp-merrow@users.noreply.github.com>
2021-08-01 21:59:50 -0400

    added code to (hopefully) remove whitespace from dealer moves

--- dealer_logic.py
+++ dealer_logic.py
@@ -7,7 +7,7 @@ with open('strategy.csv', 'r') as st:
         newData = []
         if c == 0:
             for ele in row:
-                newData.append(str(ele))
+                newData.append(str(ele).strip())
             playerLookup = newData.copy()
         else:
             for ele in row:
